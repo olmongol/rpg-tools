0ddd4e15d7f1dbac9171e3bc2bfc869e9c2e3fda
Marcus Schwamberger <marcus@lederzeug.de>
2015-10-04 12:15:59 +0200

    erstes file nur mit doxygen comments

---
+++ src/Backpack.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+\file Backpack.py
+\brief This is a simple tool to keep track of a character's backpack
+
+With this tool you are able to keep track of all the items and the money a 
+pen & paper RPG charakter is carrying. To make it work as an "Online Shop" you 
+need to set up a MySQL data base.
+
+\date (C) 2015
+\author Marcus Schwamberger
+\email marcus@lederzeug.de
+
+\todo Modul: reading and interpreting mysql configuration
+\todo Modul: create intial mysql db
+\todo Modul: insert, update and read data from db
+\todo Modul: saves extracted data as PDF
+'''
