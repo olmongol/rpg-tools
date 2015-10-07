9a0f40ebbfb1cf25c53a12a1fa3ab56826958f71
Marcus Schwamberger <marcus@lederzeug.de>
2015-10-07 19:09:05 +0200

    Baue einen einfachen EP-Berechner: calc_ep-py Baue an vereinfachten bzw dem Projekt angepaßtem window.py
    Signed-off-by: Marcus Schwamberger <marcus@lederzeug.de>

---
+++ src/calc_ep.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+\file calc_ep.py
+\package calc_ep
+\brief This is a little tool for calculating EPs for MERS/RM
+
+
+\date (C) 2015
+\author Marcus Schwamberger
+\email marcus@lederzeug.de
+
+'''
+import ConfigParser as CP
+import gui.window2
+
