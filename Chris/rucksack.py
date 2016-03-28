c9d4aca816942f880f8d20b19ac6bbe92f413749
Christian Wunderlich <cwunderlich@almandor.de>
2016-03-28 12:35:53 +0200

    Anpassung der Verzeichnisse und erste Dateien

---
+++ Chris/rucksack.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+'''
+\file rucksack.py
+\brief This is a simple tool to keep track of a character's backpack
+
+With this tool you are able to keep track of all the items and the money a
+pen & paper RPG character is carrying. To make it work as an "Online Shop" you
+need to set up a MySQL data base.
+
+\date (C) 2015
+\author Christian Wunderlich
+\email cw@almandor.de
+
+\todo Modul: alles
+
+'''
+'''
+Erst mal zum rumspielen
+'''
+
+import csv
+
