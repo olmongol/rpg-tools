f742566ec7bc3e8c5bf7bd1c4cc419b754cd6178
Marcus Schwamberger <marcus@lederzeug.de>
2016-06-15 18:03:04 +0200

    finished command line generator for random magic items
    Signed-off-by: Marcus Schwamberger <marcus@lederzeug.de>

--- src/createMagicItems.py
+++ src/createMagicItems.py
@@ -15,7 +15,7 @@ Language support exists for:
 \version 1.0
 '''
 import argparse
-from rpgToolDefinitions.helptools import dice
+from rpgtoolbox.rpgtools import dice
 
 itemTypes = {'1-40': {'de': 'Runenpapier',
                       'en': 'Rune paper'
@@ -46,15 +46,15 @@ itemTypes = {'1-40': {'de': 'Runenpapier',
                         } 
              }
 
-spellLvl = {'1-20': {'Runenpapier':'1',
-                     'Trank':'1',
-                     'Taeglich I': '1',
-                     'Taeglich II': '1',
-                     'Taeglich III': '1',
-                     'Taeglich IV': '1',
-                     'Stab': '1',
-                     'Rute': '1',
-                     'Stecken':'1'
+spellLvl = {'1-20': {'Runenpapier' :'1',
+                     'Trank'       :'1',
+                     'Taeglich I'  :'1',
+                     'Taeglich II' :'1',
+                     'Taeglich III':'1',
+                     'Taeglich IV' :'1',
+                     'Stab'        :'1',
+                     'Rute'        :'1',
+                     'Stecken'     :'1'
                      },
             '21-25': {'Runenpapier':'2',
                      'Trank'       :'1',
@@ -66,7 +66,7 @@ spellLvl = {'1-20': {'Runenpapier':'1',
                      'Rute'        :'1',
                      'Stecken'     :'2'
                      },
-            '26-30':{'Runenpapier':'2',
+            '26-30':{'Runenpapier' :'2',
                      'Trank'       :'1',
                      'Taeglich I'  :'1',
                      'Taeglich II' :'1',
@@ -76,7 +76,7 @@ spellLvl = {'1-20': {'Runenpapier':'1',
                      'Rute'        :'1',
                      'Stecken'     :'2'
                      },
-            '31-35':{'Runenpapier':'2',
+            '31-35':{ 'Runenpapier':'2',
                      'Trank'       :'2',
                      'Taeglich I'  :'1',
                      'Taeglich II' :'1',
@@ -84,9 +84,166 @@ spellLvl = {'1-20': {'Runenpapier':'1',
                      'Taeglich IV' :'1',
                      'Stab'        :'1',
                      'Rute'        :'1',
-                     'Stecken'     :'2'
+                     'Stecken'     :'3'
                      },
-            },
+            '36-40': {'Runenpapier':'2',
+                     'Trank'       :'2',
+                     'Taeglich I'  :'2',
+                     'Taeglich II' :'1',
+                     'Taeglich III':'1',
+                     'Taeglich IV' :'1',
+                     'Stab'        :'1',
+                     'Rute'        :'2',
+                     'Stecken'     :'3'
+                     },
+            '41-45': {'Runenpapier':'3',
+                     'Trank'       :'2',
+                     'Taeglich I'  :'2',
+                     'Taeglich II' :'2',
+                     'Taeglich III':'1',
+                     'Taeglich IV' :'1',
+                     'Stab'        :'1',
+                     'Rute'        :'2',
+                     'Stecken'     :'4'
+                     },
+            '46-50': {'Runenpapier':'3',
+                     'Trank'       :'2',
+                     'Taeglich I'  :'2',
+                     'Taeglich II' :'2',
+                     'Taeglich III':'2',
+                     'Taeglich IV' :'1',
+                     'Stab'        :'1',
+                     'Rute'        :'2',
+                     'Stecken'     :'4'
+                     },
+            '51-55': {'Runenpapier':'3',
+                     'Trank'       :'2',
+                     'Taeglich I'  :'3',
+                     'Taeglich II' :'2',
+                     'Taeglich III':'2',
+                     'Taeglich IV' :'1',
+                     'Stab'        :'1',
+                     'Rute'        :'2',
+                     'Stecken'     :'5'
+                     },
+            '56-60': {'Runenpapier':'4',
+                     'Trank'       :'3',
+                     'Taeglich I'  :'3',
+                     'Taeglich II' :'2',
+                     'Taeglich III':'2',
+                     'Taeglich IV' :'2',
+                     'Stab'        :'2',
+                     'Rute'        :'3',
+                     'Stecken'     :'5'
+                     },
+            '61-65': {'Runenpapier':'4',
+                     'Trank'       :'3',
+                     'Taeglich I'  :'3',
+                     'Taeglich II' :'3',
+                     'Taeglich III':'2',
+                     'Taeglich IV' :'2',
+                     'Stab'        :'2',
+                     'Rute'        :'3',
+                     'Stecken'     :'6'
+                     },
+            '66-70': {'Runenpapier':'4',
+                     'Trank'       :'3',
+                     'Taeglich I'  :'4',
+                     'Taeglich II' :'3',
+                     'Taeglich III':'2',
+                     'Taeglich IV' :'2',
+                     'Stab'        :'2',
+                     'Rute'        :'3',
+                     'Stecken'     :'7'
+                     },
+            '71-75': {'Runenpapier':'5',
+                     'Trank'       :'4',
+                     'Taeglich I'  :'4',
+                     'Taeglich II' :'3',
+                     'Taeglich III':'2',
+                     'Taeglich IV' :'2',
+                     'Stab'        :'2',
+                     'Rute'        :'3',
+                     'Stecken'     :'7'
+                     },
+            '76-80': {'Runenpapier':'5',
+                     'Trank'       :'4',
+                     'Taeglich I'  :'5',
+                     'Taeglich II' :'4',
+                     'Taeglich III':'3',
+                     'Taeglich IV' :'2',
+                     'Stab'        :'2',
+                     'Rute'        :'4',
+                     'Stecken'     :'7'
+                     },
+            '81-85': {'Runenpapier':'6',
+                     'Trank'       :'5',
+                     'Taeglich I'  :'5',
+                     'Taeglich II' :'4',
+                     'Taeglich III':'3',
+                     'Taeglich IV' :'2',
+                     'Stab'        :'2',
+                     'Rute'        :'4',
+                     'Stecken'     :'8'
+                     },
+            '86-90': {'Runenpapier':'7',
+                     'Trank'       :'6',
+                     'Taeglich I'  :'6',
+                     'Taeglich II' :'5',
+                     'Taeglich III':'3',
+                     'Taeglich IV' :'3',
+                     'Stab'        :'2',
+                     'Rute'        :'4',
+                     'Stecken'     :'8'
+                     },
+            '91-94': {'Runenpapier':'8',
+                     'Trank'       :'7',
+                     'Taeglich I'  :'7',
+                     'Taeglich II' :'5',
+                     'Taeglich III':'4',
+                     'Taeglich IV' :'3',
+                     'Stab'        :'2',
+                     'Rute'        :'5',
+                     'Stecken'     :'9'
+                     },
+            '95-97': {'Runenpapier':'9',
+                     'Trank'       :'8',
+                     'Taeglich I'  :'8',
+                     'Taeglich II' :'6',
+                     'Taeglich III':'4',
+                     'Taeglich IV' :'3',
+                     'Stab'        :'2',
+                     'Rute'        :'5',
+                     'Stecken'     :'9'
+                     },
+            '98-99': {'Runenpapier':'10',
+                     'Trank'       :'9',
+                     'Taeglich I'  :'9',
+                     'Taeglich II' :'7',
+                     'Taeglich III':'5',
+                     'Taeglich IV' :'3',
+                     'Stab'        :'2',
+                     'Rute'        :'5',
+                     'Stecken'     :'10'
+                     },
+            '100-100': { 'Runenpapier' :'10',
+                         'Trank'       :'10',
+                         'Taeglich I'  :'10',
+                         'Taeglich II' :'7',
+                         'Taeglich III':'5',
+                         'Taeglich IV' :'3',
+                         'Stab'        :'2',
+                         'Rute'        :'5',
+                         'Stecken'     :'10'
+                       },
+            }
+
+##
+# This holds the number of loaded spells in an item            
+loadedSpells = {'Stab' : 10,
+               'Rute' : 30,
+               'Stecken' : 100
+               }
 
 spellRealms = {'1-30': {'de': 'Offene Essenz',
                          'en': 'Open Essence'
@@ -127,10 +284,10 @@ spellLists = {'1-2' : {'Offene Essenz': {'de': 'Fluch',
                                   'en': 'Fire Law'
                                   },
                        'Leitmagie': {'de': 'Naturkunde',
-                                     'en': 'Nature´s law'
+                                     'en': 'Natures law'
                                      },
                        'Animisten': {'de': 'Seelenkunde',
-                                     'en': 'Soul´s law'
+                                     'en': 'Souls law'
                                      },
                        'Barde/Waldlaeufer' : {'de': 'Wege des Lernens',
                                               'en': 'Ways of learning'
@@ -269,6 +426,8 @@ spellLists = {'1-2' : {'Offene Essenz': {'de': 'Fluch',
 
 
 
+
+
 parser = argparse.ArgumentParser(description = "Magical Items Creator")
 parser.add_argument('-n', '--number', action = 'store', type = int, default = 1,
                     help = 'Anzahl der zu erzeugenden Gegenstaende')
@@ -282,9 +441,97 @@ args = parser.parse_args()
 n = args.number
 t = args.type
 fn = args.out
+lang = args.language
+fcontent = "Magische Gegenstaende:\n----------------------\n\n"
 
 for i in range(1, n):
     
     if t == 0:
-        print "o"
+        roll = dice(100, 4)
+        
+        for key1 in itemTypes.keys():
+            dummy = key1.split('-')
+ 
+            if int(dummy[0]) <= roll[0] <= int(dummy[1]):
+                item = itemTypes[key1]['de']
+                fcontent += "- " + itemTypes[key1][lang]
+            
+                if  item in loadedSpells.keys():
+                    fcontent += " (" + str(dice(loadedSpells[item], 1)) + "/" + str(loadedSpells[item]) + ")"
+                
+                del(dummy)    
+                break
+            
+        for key2 in spellRealms.keys():
+            dummy = key2.split('-') 
+            
+            if int(dummy[0]) <= roll[1] <= int(dummy[1]):
+                realm = spellRealms[key2]['de']
+                fcontent += " " + spellRealms[key2][lang] + ":"
+                del(dummy)
+                break
+            
+        for key3 in spellLists.keys():
+            dummy = key3.split('-')
+            
+            if int(dummy[0]) <= roll[2] <= int(dummy[1]):
+                fcontent += ' ' + spellLists[key3][realm][lang] 
+                del(dummy)
+                break
+            
+        for key4 in spellLvl.keys():
+            dummy = key4.split('-')
+            
+            if int(dummy[0]) <= roll[3] <= int(dummy[1]):
+                fcontent += " [lvl " + spellLvl[key4][item] + "]\n"
+                del(dummy)
+                break
+    
+    else:
+        roll = dice(100, 3)
+
+        for key1 in itemTypes.keys():
+            dummy = key1.split('-')
+ 
+            if int(dummy[0]) <= t <= int(dummy[1]):
+                item = itemTypes[key1]['de']
+                fcontent += "- " + itemTypes[key1][lang]
+            
+                if  item in loadedSpells.keys():
+                    fcontent += " (" + str(dice(loadedSpells[item], 1)) + "/" + str(loadedSpells[item]) + ")"
+                
+                del(dummy)    
+                break
+            
+        for key2 in spellRealms.keys():
+            dummy = key2.split('-') 
+            
+            if int(dummy[0]) <= roll[0] <= int(dummy[1]):
+                realm = spellRealms[key2]['de']
+                fcontent += " " + spellRealms[key2][lang] + ":"
+                del(dummy)
+                break
+            
+        for key3 in spellLists.keys():
+            dummy = key3.split('-')
+            
+            if int(dummy[0]) <= roll[1] <= int(dummy[1]):
+                fcontent += ' ' + spellLists[key3][realm][lang] 
+                del(dummy)
+                break
+            
+        for key4 in spellLvl.keys():
+            dummy = key4.split('-')
+            
+            if int(dummy[0]) <= roll[2] <= int(dummy[1]):
+                fcontent += " [lvl " + spellLvl[key4][item] + "]\n"
+                del(dummy)
+                break        
+        
+                
+fp = open(fn, "w")
+fp.write(fcontent)
+fp.close()
+
+print "Datei %s erstellt!" % (fn)              
     
