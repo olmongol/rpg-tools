43406a97f2e1c4b3f719f14fdd47b9b9d4ffa1cf
Marcus Schwamberger <marcus@lederzeug.de>
2019-05-05 13:09:47 +0200

    started with output LaTeX modules

--- tests/print_char.py
+++ tests/print_char.py
@@ -2,6 +2,8 @@
 import json
 from pprint import pprint
 import string
+fpath="../src/data/default/latex/"
+fnames=['template_charsheet.tex', 'template_trainpack.tex', 'template_test.tex', 'template_cats.tex', 'template_stats.tex', 'template_spells.tex', 'template_gen_info.tex', 'template_rr_at_db.tex', 'template_skills.tex']
 
 class myDelimiter(string.Template):
     delimiter="==>"
@@ -74,3 +76,93 @@ def buildRRTable(char={}):
 
     return rpl_table
             
+def buildGenTable(char={},latexpath="./"):
+    """
+    """
+    entries = ["prof","race","culture","hometown","lord","parents","siblings", \
+               "partner","children","deity","gender","skin","eyes","hair","size",\
+               "weight","apparentage","actualage","looking","souldeparture",\
+               "recovery","lvl","jpg","description","motivation","realm","exp","name"]
+    rpl_table = {}
+    for elem in entries:
+        if elem in list(char.keys()):
+            rpl_table[str(elem)]=str(char[elem])
+        elif elem == "jpg":
+            rpl_table[str(elem)] = latexpath+"default.jpg"
+        else:
+            rpl_table[str(elem)] = " "
+
+    return rpl_table
+
+def buildCatSkill(char={}):
+    """
+    """
+    colors = {"Weapon" : "Red",
+              "Magic": "Blue",
+              "Develop" : "Green",
+              "Standard" : "Black"
+              }
+    table_line = "\textcolor{==>color}{==>cat} & \textcolor{==>color}{==>Skill} & \textcolor{==>color}{==>rank} & " + \
+                 "\textcolor{==>color}{==>rank_bonus ==>prof_bonus} & \textcolor{==>color}{==>spec_bonus} & " +\
+                 "\textcolor{==>color}{==>item_bonus} &\textcolor{==>color}{total_bonus}"
+    tpl = myDelimiter(table_line)
+    output_tbl = ""
+    
+    for cat in list(char["cat"].keys()):
+        if "Weapon" in cat:
+            color = colors["Weapon"]
+
+        elif "Development" in cat:
+            color = colors["Develop"]
+            
+        elif "Spell" in cat or "Power" in cat:
+            color = colors["Magic"]
+
+        else:
+            color = colors["Standard"]
+          
+        if cat not in ["Progression"]:
+           
+            rplm = {"Skill": "",
+                    "cat": cat,
+                    "rank": char["cat"][cat]["rank"],
+                    "rank_bonus" : char["cat"][cat]["rank bonus"],
+                    "spec_bonus" : char["cat"][cat]["spec bonus"],
+                    "item_bonus" : char["cat"][cat]["item bonus"],
+                    "total_bonus": char["cat"][cat]["total bonus"],
+                    "color": color
+                    }
+            if "prof bonus" in list(char["cat"][cat].keys()):
+                rplm["prof_bonus"]=char["cat"][cat]["prof bonus"]
+            else:
+                rplm["prof_bonus"]= "--"
+                
+            output_tbl += tpl.safe_substitute(rplm)
+        
+        for skill in list(char["cat"][cat]["Skill"].keys()):
+            print("{} : {} --> \n\t{} : {}".format(cat,skill,char["cat"][cat],char["cat"][cat]["Skill"][skill]))
+            if skill not in ["Progression"]:
+                rplm ={
+                        "Skill": skill,
+                        "cat": "--",
+                        "rank": char["cat"][cat]["Skill"][skill]["rank"],
+                        "rank_bonus" : char["cat"][cat]["Skill"][skill]["rank bonus"],
+                        "prof_bonus" : "--",
+                        "spec_bonus" : char["cat"][cat]["Skill"][skill]["spec bonus"],
+                        "item_bonus" : char["cat"][cat]["Skill"][skill]["item bonus"],
+                        "total_bonus": char["cat"][cat]["Skill"][skill]["total bonus"],
+                        "color" : color
+                    }
+                output_tbl += tpl.safe_substitute(rplm)
+                
+    return output_tbl
+        
+def buildSkillTable(char={}):
+    """
+    """
+    colors = {"Weapon" : "==>color",
+              "Magic": "Blue",
+              "Develop" : "Green",
+              "Standard" : "Black"
+              }
+    table_line = "\textcolor{==>color}{==>skill} & \textcolor{==>color}{==>category} & \textcolor{==>color}{00} & \textcolor{==>color}{00} & \textcolor{==>color}{00} & \textcolor{==>color}{00} &\textcolor{==>color}{00}"
