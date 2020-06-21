4a1e5a950ff15769e93a7d10e37ee594e130d167
Marcus Schwamberger <marcus@lederzeug.de>
2020-06-21 19:31:45 +0200

    fix 2 services key

--- src/rm-inventory.py
+++ src/rm-inventory.py
@@ -556,8 +556,8 @@ class shopWin(blankWindow):
         self.character = char
         if "inventory" in self.character.keys():
             self.inv_char = self.character["inventory"].copy()
-            if "services" not in self.inv_char.keys():
-                self.inv_char["services"] = []
+            if "services" not in self.character["inventory"].keys():
+                self.character["inventory"]["services"] = []
 
         else:
             self.inv_char = {'weapon' :[],
