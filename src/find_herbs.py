6d38803c727a2e8b11c93a429dd8a47be960ba2e
Marcus Schwamberger <marcus@lederzeug.de>
2020-08-30 10:44:55 +0200

    filled herb's table

--- src/find_herbs.py
+++ src/find_herbs.py
@@ -1,18 +1,33 @@
+#!/usr/bin/python3
+'''
+\file find_herbs.py
+\brief This is a little tool for finding herbs in different areas of Middle-Earth
+
+
+
+\date (C) 2020
+\author Marcus Schwamberger
+\email marcus@lederzeug.de
+\license GNU V3.0
+\version 1.1.0
+
+'''
+
 import json
 import csv
 from pprint import pprint
 from rpgtoolbox.globaltools import readCSV
-#from gui.spelleditor import readCSV
 from rpgtoolbox.rpgtools import statManeuver as statMan
 from rpgtoolbox.rpgtools import dice
 from rpgToolDefinitions import inventory as inv
 from rpgToolDefinitions.epcalcdefs import maneuvers as manmod
-#from rpgToolDefinitions.inventory import coins
 
-defaultpath="data/default/inventory/"
+defaultpath = "data/default/inventory/"
 herbsfile = "herbs.csv"
 
-herbs = readCSV(defaultpath+herbsfile)
+herbs = readCSV(defaultpath + herbsfile)
+
+
 
 def string2worth(worth = ""):
     '''
@@ -23,7 +38,7 @@ def string2worth(worth = ""):
 ##    print(worth)
     result = inv.money.copy()
     value = worth.split(" ")
-    
+
     for v in value:
         pos = 0
         pos = inv.coins["short"].index(v[-2:])
@@ -31,20 +46,23 @@ def string2worth(worth = ""):
 
     return result
 
-def sumDices(sides=4,number=1):
+
+
+def sumDices(sides = 4, number = 1):
     """
     Rolls number D sides and returns the sum of the results
     @param sides of the dice to roll
     @param number of the dices to roll
     """
-    result=0
-    for r in dice(sides,number):
-        result +=r
+    result = 0
+    for r in dice(sides, number):
+        result += r
 
     return result
 
 
-def findHerbs(roll=0,skill=-15,area="---",climate="mild temperate",locale=["---"]):
+
+def findHerbs(roll = 0, skill = -15, area = "---", climate = "mild temperate", locale = ["---"]):
     """
     This function searches for herbs by area, climate and locale dependend on difficulty of
     finding and the success of the skill (foraging) roll.
@@ -55,9 +73,9 @@ def findHerbs(roll=0,skill=-15,area="---",climate="mild temperate",locale=["---"
     @param locale local environment conditions like 'desert'
     @retval result list of dictionaries holding the found herbs
     """
-    herbs = readCSV(defaultpath+herbsfile)
+    herbs = readCSV(defaultpath + herbsfile)
     result = []
-    protoherb ={"name":"",
+    protoherb = {"name":"",
                 "AF" : 0,
                 "climate":"",
                 "description":"",
@@ -75,42 +93,38 @@ def findHerbs(roll=0,skill=-15,area="---",climate="mild temperate",locale=["---"
                 "other name":[],
                 "found":[]
             }
-    
+
     statman = statMan()
     for plant in herbs:
-        plant["name"]=plant.pop("item")
-        plant["description"]=plant.pop("comment")
-        plant["medical use"]=plant.pop("effect")
-        plant["worth"]=plant.pop("cost")
-        plant["worth"]=string2worth(plant["worth"])
-        plant["weight"]=0.1
-        plant["location"]="equipped"
-        plant["other use"]=""
-        plant["other name"]=[]
-        
-        
-        if plant["area"] in [area,"everywhere"] and plant["climate"]==climate and plant["locale"]==locale:
+        plant["name"] = plant.pop("item")
+        plant["description"] = plant.pop("comment")
+        plant["medical use"] = plant.pop("effect")
+        plant["worth"] = plant.pop("cost")
+        plant["worth"] = string2worth(plant["worth"])
+        plant["weight"] = 0.1
+        plant["location"] = "equipped"
+        plant["other use"] = ""
+        plant["other name"] = []
+
+        if plant["area"] in [area, "everywhere"] and plant["climate"] == climate and plant["locale"] == locale:
             mod = manmod[plant["difficulty"]]["mod"]
             if roll != 100 and roll != 66:
-                check = statman.checkRoll(roll+skill+mod)
+                check = statman.checkRoll(roll + skill + mod)
             else:
                 check = statman.checkRoll(roll)
 ##            pprint(check)
-            
+
             if check["success"] == "1.0":
                 no = 1
             elif check["success"] == "1.25":
-                no = sumDices(5,1)
+                no = sumDices(5, 1)
             elif check["success"] == "1.2":
                 no = 2
             else:
-                no=0
+                no = 0
 
-            for i in range(0,no):
+            for i in range(0, no):
                 result.append(plant)
 
     return result
-            
-                
-            
 
