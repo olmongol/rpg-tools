42f8b93132956ebbff5e97c18d0b87d286b209bc
Marcus Schwamberger <marcus@lederzeug.de>
2020-10-24 17:10:47 +0200

    tool for entering crit tabs

--- src/storecrittabs.py
+++ src/storecrittabs.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 import json
+from pprint import pprint
 
-def readJSON(filename):
-    with open(filename,"r") as fp:
-        cont = json.read(fp)
-    return cont
-
-def writeJSON(filename, cont):
-    with open(filename,"w") as fp:
-        json.dump(cont,fp,indent=4)
-
-    print("{} saved".format(filename))
-    
 datadir="./data/default/fight/crits/"
 tempdic = {"description": "",
+           "initiative": 0,
             "hits": 0,
             "hits/rnd": 0,
             "kill": -1,
@@ -22,5 +13,70 @@ tempdic = {"description": "",
             "mod foe": 0,
             "no parry": 0,
             "parry": 0,
-            "stunned": 0
+            "stunned": 0,
+           "damaged" : ""
             }
+
+limits = ['5', '10', '15', '20', '35', '45', '50', '55', '60', '65', '66', '70', '75', '80', '85', '90', '95', '99', '100']
+crits = ["A","B","C","D","E"]
+
+def readJSON(filename):
+    with open(filename,"r") as fp:
+        cont = json.load(fp)
+    return cont
+
+def writeJSON(filename, cont):
+    with open(filename,"w") as fp:
+        json.dump(cont,fp,indent=4)
+
+    print("{} saved".format(filename))
+    
+def editCont(fcont={},filename="test"):
+    cont = dict.copy(fcont)
+    
+    if limits != list(cont.keys()):
+        #cont = dict.fromkeys(limits,dict.fromkeys(crits,{}))
+        cont = {}
+        for l in limits:
+            cont[l]={}
+            for crit in crits:
+                cont[l][crit]={}
+
+    for dice in cont.keys():
+        
+        for crit in cont[dice].keys():
+            
+            if cont[dice][crit] == {}:
+                cont[dice][crit]["default"]= dict.copy(tempdic)
+                cont[dice][crit]["alternate"]=None
+                
+            for stat in cont[dice][crit].keys():
+                
+                if stat == "alternate" and cont[dice][crit]["alternate"]==None:
+                    altn = input("alternate (j/N): ")
+                                        
+                    if altn in ["j","J","y","Y"]:
+                        cont[dice][crit]["alternate"]= dict.copy(tempdic)
+                        
+                if cont[dice][crit][stat]:
+                    
+                    for param in cont[dice][crit][stat].keys():
+                        
+                        output ="{}/{}/{}/{}: {} => ".format(dice,crit,stat,param,cont[dice][crit][stat][param])
+                   
+                        enter = input(output)
+                        
+                        if enter!="":
+                            cont[dice][crit][stat][param] = "{:s}".format(enter)
+
+                       
+                        
+            writeJSON(datadir+filename,cont)            
+
+        print("\n\n")
+
+    
+    return cont
+
+#      krush = readJSON(datadir+"Krush.json")          
+#      krush=editCont(krush,"Krush.json")  
