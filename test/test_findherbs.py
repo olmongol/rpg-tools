529e3325d2fc4272cd85ef7846781c8aad73102b
Nillorian <weonly@gmx.net>
2022-11-27 21:57:54 +0100

    first try of two testcases

--- test/test_findherbs.py
+++ test/test_findherbs.py
@@ -1,21 +1,50 @@
 import unittest
 
 from findherbs import findHerbs
+from findherbs import loadHerbs
 
 '''
 This is first experimentaton for testing the module findherbs
 
-Findings:
+You have to be in ./src folder to start it
+
+----
+@todo add better given statements to set up tests
+@todo add constants for realistic roles and skills
+@todo improve error messages 
+
 '''
 
 class TestFindherbs(unittest.TestCase):
-    def test_shouldFindHerbsWithArea(self):
-        print("test_shouldFindHerbsWithArea")
-        areyWhereIsSearched = "";
-        result = findHerbs(roll=200, area=areyWhereIsSearched)
-        print(result);  
+    herbsList = loadHerbs(path=".\data\default\inventory\herbs.csv")
+
+    def test_shouldFindHerbsIn(self, list=herbsList):
+        # given
+        searcherHasNoSkill = -15
+        rollIsMaximum = 200 
+        areyWhereIsSearched = ["Arnor"]
+        climateWhereIsSearched = ["cold"]
+        localeWhereIsSearched = ["mountains"]
+
+        #when
+        result = findHerbs(list, skill=searcherHasNoSkill, roll=rollIsMaximum, area=areyWhereIsSearched, climate=climateWhereIsSearched, locale=localeWhereIsSearched)
         
-        self.assertGreater(result, 1)
+        #then
+        self.assertTrue(msg="The result should have at least a result", expr=(result != []) )
+
+    def test_shouldFindHerbsIn(self, list=herbsList):
+        # given
+        searcherHasNoSkill = -15
+        rollIsMaximum = 200 
+        areyWhereIsSearched = ["Dagorlad"]
+        climateWhereIsSearched = ["cold"]
+        localeWhereIsSearched = ["mountains"]
+
+        #when
+        result = findHerbs(list, skill=searcherHasNoSkill, roll=rollIsMaximum, area=areyWhereIsSearched, climate=climateWhereIsSearched, locale=localeWhereIsSearched)
+        
+        #then
+        self.assertTrue(msg="The result should have at least a result", expr=(result != []) )
 
 if __name__ == '__main__':
     unittest.main()
