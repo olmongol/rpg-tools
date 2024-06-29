2a8a5bbfe2e28a07f756f126a05204a3a2c8cad4
Nillorian <weonly@gmx.net>
2024-06-29 11:39:09 +0200

    new test

--- test/test_findherbs.py
+++ test/test_findherbs.py
@@ -8,6 +8,18 @@ This is first experimentaton for testing the module findherbs
 
 You have to be in ./src folder to start it
 
+roll	classification
+ < -26	Spectacular Failure
+-25 < 04	Absolute Failure
+05 < 75	Failure
+UM < 66	Unusual Event
+76 < 90	Partial Success
+91 < 110	Near Success
+UM < 100	Unusual Success
+111 < 175	Success
+176 < 	Absolute Success
+
+
 ----
 @todo add better given statements to set up tests
 @todo add constants for realistic roles and skills
@@ -16,7 +28,7 @@ You have to be in ./src folder to start it
 '''
 
 class TestFindherbs(unittest.TestCase):
-    herbsList = loadHerbs(path=".\data\default\inventory\herbs.csv")
+    herbsList = loadHerbs(path="./data/default/inventory/herbs.csv")
 
     def test_shouldFindHerbsIn(self, list=herbsList):
         # given
@@ -39,6 +51,22 @@ class TestFindherbs(unittest.TestCase):
         areyWhereIsSearched = ["Dagorlad"]
         climateWhereIsSearched = ["cold"]
         localeWhereIsSearched = ["mountains"]
+
+        #when
+        result = findHerbs(list, skill=searcherHasNoSkill, roll=rollIsMaximum, area=areyWhereIsSearched, climate=climateWhereIsSearched, locale=localeWhereIsSearched)
+        
+        #then
+        self.assertTrue(msg="The result should have at least a result", expr=(result != []) )
+
+#Thurl	Concussion Relief	northeastern Eriador	cool temperate	decidous/mixed forest	routine	Clove	Brew	3tp	0		Heals 1-4 hit points. Brew keeps 1-2 weeks.
+
+    def test_shouldFindHerbsIn(self, list=herbsList):
+        # given
+        searcherHasNoSkill = 50
+        rollIsMaximum = 50
+        areyWhereIsSearched = ["northeastern Eriador"]
+        climateWhereIsSearched = ["cool temperate"]
+        localeWhereIsSearched = ["decidous/mixed forest"]
 
         #when
         result = findHerbs(list, skill=searcherHasNoSkill, roll=rollIsMaximum, area=areyWhereIsSearched, climate=climateWhereIsSearched, locale=localeWhereIsSearched)
